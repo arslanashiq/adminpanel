03dbcc8b605d1047bb1a387d47c23bb9217717d3
Muhammad <ashiqarslan66@gmail.com>
2022-06-23 06:14:33 +0500

    del order set

--- src/Constants/DataBaseURL.js
+++ src/Constants/DataBaseURL.js
@@ -1,4 +1,4 @@
 export const URL={
-    // My_Database_Url:"https://spicy-food-api.herokuapp.com/api/v1/",
-    My_Database_Url:"http://localhost:3000/api/v1/",
+    My_Database_Url:"https://spicy-food-api.herokuapp.com/api/v1/",
+    // My_Database_Url:"http://localhost:3000/api/v1/",
 }
